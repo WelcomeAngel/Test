c857db549141565912e2b44210f2f3bdcc11989d
804020204 <804020204@qq.com>
2018-12-11 17:03:41 +0800

    test

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp7
         static void Main(string[] args)
         {
             Console.WriteLine("1233");
+            Console.Read();
         }
 
         static void Test()
